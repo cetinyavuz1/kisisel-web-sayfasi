44cdadb99c7de162f9844f5624d9b914f548acf7
cetinyavuz1 <cetinyavuz1@hotmail.com>
2025-05-27 02:07:08 +0300

    dark mode css'i tamamlandı

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,21 +11,19 @@ export default function Projects() {
             <div className="projects-background">
                 <div className="projects-container">
                     <h2 className="projects-header">{languageData.projects.title}</h2>
-                    <div className="projects">
-                        <div className="projects-project">
-                            <img className="projects-img" src={languageData.projects.projectsImg} />
-                            <div className="projects-pizza">
-                                <h3 className="projects-header2">{languageData.projects.projectsHeader}</h3>
-                                <p className="projects-text">{languageData.projects.projectsText}</p>
-                                <div className="projects-tools">
-                                    <p className="projects-tools-single">react</p>
-                                    <p className="projects-tools-single">redux</p>
-                                    <p className="projects-tools-single">vercel</p>
-                                </div>
-                                <div className="projects-links">
-                                    <a className="links-style" href={languageData.projects.siteURL} target="_blank">{languageData.projects.site}</a>
-                                    <a className="links-style" href={languageData.projects.githubURL} target="_blank">{languageData.projects.github}</a>
-                                </div>
+                    <div className="projects-project">
+                        <img className="projects-img" src={languageData.projects.projectsImg} />
+                        <div className="projects-pizza">
+                            <h3 className="projects-header2">{languageData.projects.projectsHeader}</h3>
+                            <p className="projects-text">{languageData.projects.projectsText}</p>
+                            <div className="projects-tools">
+                                <p className="projects-tools-single">react</p>
+                                <p className="projects-tools-single">redux</p>
+                                <p className="projects-tools-single">vercel</p>
+                            </div>
+                            <div className="projects-links">
+                                <a className="links-style" href={languageData.projects.siteURL} target="_blank">{languageData.projects.site}</a>
+                                <a className="links-style" href={languageData.projects.githubURL} target="_blank">{languageData.projects.github}</a>
                             </div>
                         </div>
                     </div>
