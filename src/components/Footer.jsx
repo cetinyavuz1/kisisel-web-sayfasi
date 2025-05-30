97b2bafd9f00837198f702e2ccd9687517c642f1
cetinyavuz1 <cetinyavuz1@hotmail.com>
2025-05-30 06:48:22 +0300

    css iyileştirmeleri yapıldı

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,14 +8,16 @@ export default function Footer() {
 
     return (
         <>
-            <div className="footer-container">
-                <h2 className="footer-header">{languageData.footer.title}</h2>
-                <p className="footer-text">{languageData.footer.text}</p>
-                <p className="footer-email">{languageData.footer.email}</p>
-                <div className="footer-icons">
-                    {languageData.footer.icons.map((elm, index) => {
-                        return <a href={elm.link} key={index} target="_blank"><i className={elm.class}></i></a>
-                    })}
+            <div className="footer-background">
+                <div className="footer-container">
+                    <h2 className="footer-header">{languageData.footer.title}</h2>
+                    <p className="footer-text">{languageData.footer.text}</p>
+                    <p className="footer-email">{languageData.footer.email}</p>
+                    <div className="footer-icons">
+                        {languageData.footer.icons.map((elm, index) => {
+                            return <a href={elm.link} key={index} target="_blank"><i className={elm.class}></i></a>
+                        })}
+                    </div>
                 </div>
             </div>
         </>
